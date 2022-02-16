2f1c1c13c88e2ef2615095c7a05e88c50ea715af
byaskew3 <byaskew3@gmail.com>
2022-02-16 11:40:28 -0700

    JS/finished project

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('people counting')
+let counterEl = document.querySelector('.counter-el');
+let count = 0;
+
+let entryEl = document.querySelector('.entry-el');
+
+function increment() {
+    count += 1;
+    counterEl.textContent = count;
+    console.log(counterEl);
+}
+
+function decrement() {
+    count -= 1;
+    counterEl.textContent = count;
+    console.log(counterEl);
+}
+
+function save() {
+    `${entryEl.textContent += counterEl.textContent += ', '}`;
+    count = 0;
+    counterEl.textContent = count;
+}
